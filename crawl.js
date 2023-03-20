0fff0da87d459bcd9a4beafd1781599d81ad601c
sengunoglud <65671129+sengunoglud@users.noreply.github.com>
2023-03-20 13:21:20 +0300

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,6 +1,26 @@
 const { link } = require('fs/promises')
 const {JSDOM} = require('jsdom') 
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`)
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`error in fetch with status code:  ${resp.status}, on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`non html response, content type: ${contentType}`)
+            return
+        }
+        console.log(await resp.text())
+    } catch(err) {
+        console.log(`error in fetch:  ${err.message}, on page: ${currentURL}`)
+    }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -42,5 +62,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Hello world")
+const {crawlPage} = require("./crawl.js")
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("no website provided.")
+        process.exit(1)  //basic error code, something gone wrong
+    }
+
+    if (process.argv.length > 3) {
+        console.log("too many command line args")
+        process.exit(1)  //basic error code, something gone wrong
+    }
+
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl of ${baseURL}`)
+    crawlPage(baseURL)
+
+}
+
+
+main()
